03e319be65dc9342e888d8001c2b9a549e78eca2
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2024-01-28 20:50:45 +0300

    card component created
    card component created

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectCardComponent } from "../project-card/project-card.component";
 
 @Component({
-  selector: 'app-portfolio',
-  standalone: true,
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css',
+    selector: 'app-portfolio',
+    standalone: true,
+    templateUrl: './portfolio.component.html',
+    styleUrl: './portfolio.component.css',
+    imports: [ProjectCardComponent]
 })
 export class PortfolioComponent {
   constructor(private titleService: Title) {
