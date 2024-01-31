c46de74d0e00c6b1aefff43ab06a81a5a5e74ec0
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2024-01-31 10:26:55 +0300

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,29 @@ import { NavComponent } from './nav/nav.component';
 import { CommonModule } from '@angular/common';
 import { NgbAccordionModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ResumeComponent } from "./resume/resume.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    HeaderComponent,
-    NavComponent,
-    NgbModule,
-    FormsModule,
-    NgbAccordionModule,
-  ],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        HeaderComponent,
+        NavComponent,
+        NgbModule,
+        FormsModule,
+        NgbAccordionModule,
+        HomeComponent,
+        PortfolioComponent,
+        ResumeComponent,
+        ContactComponent
+    ]
 })
 export class AppComponent {
   title = 'angular-project';
